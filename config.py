d1c6ad38fff7edf8c9309aaf5ee272857086e526
jimmybutton <samu.liedtke@gmail.com>
2020-04-21 22:45:10 +0100

    user login

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '984e18a7-8595-4bb4-bcb2-8f530aa8912f'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "984e18a7-8595-4bb4-bcb2-8f530aa8912f"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- moviedb.py
+++ moviedb.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app, db
+from app.models import User, Movie
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Movie': Movie}
